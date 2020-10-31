e6801c79231ce25c40a5cd7e4ebee6a9778c87ff
Huu loc <1751010078loc@ou.edu.vn>
2020-10-31 08:24:25 +0700

    thaydoicode

--- src/btth/Bth3Controller.java
+++ src/btth/Bth3Controller.java
@@ -44,8 +44,8 @@ public class Bth3Controller implements Initializable {
         chart.setTitle( " Bieu Do  " );
         
         for( Node n: vctrl.getChildren()){
-            TextField t = (TextField) n;
-            chart.getData().add(new PieChart.Data(t.getText(),Integer.parseInt(t.getText())));
+            TextField d = (TextField) n;
+            chart.getData().add(new PieChart.Data(d.getText(),Integer.parseInt(d.getText())));
     }
         sp.getChildren().add(chart);
 }}
